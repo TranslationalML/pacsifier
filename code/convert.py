cdff41ce4839fd34844f1085f2986be2b9dcbb9e
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2018-08-30 16:12:05 +0200

    Cloned heudiconv and dcmstack / Added utils folder with scripts (1) for generating miniconda environment from a spec_file also uploaded (2) for installing local clones of dcmstack and heudiconv in the environment / minor change of the path for convertall.py and heuristic.py

--- code/convert.py
+++ code/convert.py
@@ -5,22 +5,22 @@ from tqdm import tqdm
 import sys
 import nipype
 
-def list_files(path : str) -> list : 
+def list_files(path : str) -> list :
 	"""
 	returns a list of all files within path passed as parameter.
-	Args : 
+	Args :
 		path (string) :directory path
-	Returns : 
+	Returns :
 		list : list of paths.
 	"""
 	return glob(path)
 
-def process_list(paths : list) -> list: 
+def process_list(paths : list) -> list:
 	"""
 	Extracts subject and session identifiers as stored in data folder.
-	Args : 
+	Args :
 		paths (list) : list of paths
-	Returns : 
+	Returns :
 		list : list of tuples like (subject, session).
 	"""
 	tuples = [(path.split("/")[-2].split("-")[-1], path.split("/")[-1].split("-")[-1]) for path in paths]
@@ -29,7 +29,7 @@ def process_list(paths : list) -> list:
 def convert(subject : str, session : str , base : str) :
 	"""
 	Converts the subject and session in base directory corresponding dicom files to nifti.
-	Args : 
+	Args :
 		subject (string) : subject identifier
 		session (string) : session identifier
 		base (string) : directory path
@@ -42,16 +42,19 @@ def convert(subject : str, session : str , base : str) :
 	-s 2936187 -ss 20171013133317 \
 	-c dcm2niix -b --overwrite
 	"""
-	
+
+	#Get the directory where this file is (PACSMANS/code)
+	dirname = os.path.dirname(__file__)
+
 	command = "heudiconv -d  {}/sub-{}/ses-{}/*/* -o {} -f {} -s {} -ss {} -c none --overwrite".format(
 		base,
 		"{subject}",
 		"{session}",
 		os.path.join(base,"Nifti"),
-		os.path.join(base,"..","code","convertall.py"),
+		os.path.join(dirname,"convertall.py"),
 		subject,
 		session)
-	
+
 	global_path = os.path.join(base, "sub-*","ses-*","*","*")
 	print(command)
 	teff = run(command)
@@ -62,43 +65,43 @@ def convert(subject : str, session : str , base : str) :
 		"{subject}",
 		"{session}",
 		os.path.join(base,"Nifti"),
-		os.path.join(base,"..","code","heuristic.py"),
+		os.path.join(dirname,"heuristic.py"),
 		subject,
 		session)
-	
+
 	global_path = os.path.join(base, "sub-*","ses-*","*","*")
 	print(command)
 	teff = run(command)
 	print(teff)
-	
+
 	paths = glob(global_path)
-	
+
 	# for i, path in enumerate(paths):
-		
+
 	# 	c3d_command = 'python c3d_nipype.py '+ path  +" "+ os.path.join(base,"Nifti", str(i)+'.nii.gz')
 	# 	teff = run(c3d_command)
 
 def convert_all(base) :
 	"""
 	Converts all dicom files within base directory to nifti files
-	Args : 
+	Args :
 		base (string) : base directory path.
 	"""
-	
+
 	global_path = os.path.join(base, "sub-*","*")
 
 	paths = list_files(global_path )
 	tuples = process_list(paths)
-	
+
 	abs_path = os.path.abspath(base)
-	
+
 	print("Converting dicom files to nifti")
 	for tuple_ in tqdm(tuples) :
 		convert(tuple_[0], tuple_[1], abs_path)
 
 
-def main(argv) : 
+def main(argv) :
 	convert_all(argv[0])
 
-if __name__ == "__main__" : 
+if __name__ == "__main__" :
 	main(sys.argv[1:])
