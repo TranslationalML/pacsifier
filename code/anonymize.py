66444d1cb95a321bc476b59e686fe9e0072872e5
firas11 <firas.benothman@epfl.ch>
2018-09-06 13:31:55 +0200

    type hints

--- code/anonymize.py
+++ code/anonymize.py
@@ -6,8 +6,9 @@ import os
 from tqdm import tqdm
 import random
 import json
+from typing import Dict
 
-def fuzz_date(date, fuzz_parameter = 60): 
+def fuzz_date(date : str, fuzz_parameter : int = 60) -> str: 
 	"""
 	Fuzzes date in a range of fuzz_parameter days prior to fuzz_parameter days after.
 	Args : 
@@ -29,7 +30,11 @@ def fuzz_date(date, fuzz_parameter = 60):
 	date = (date + timedelta(days = fuzz)).strftime("%Y%m%d")
 	return date
 
-def anonymize(filename, output_filename, PatientID = "test1", PatientName = "test1"):
+def anonymize(
+	filename : str,
+	output_filename : str, 
+	PatientID : str = "test1",
+	PatientName : str = "test1"):
 	"""
 	Anonymizes the dicom image located at filename by affecting  patient id, patient name and date.
 	Args : 
@@ -52,8 +57,11 @@ def anonymize(filename, output_filename, PatientID = "test1", PatientName = "tes
 	# write the 'anonymized' DICOM out under the new filename
 	dataset.save_as(output_filename)
 
-def anonymize_all(output_folder = ".",datapath = os.path.join("..","data"), subject_dicom_path = os.path.join("ses-*","*","*")):
-		"""
+def anonymize_all(
+	output_folder : str = ".",
+	datapath : str  = os.path.join("..","data"),
+	subject_dicom_path : str = os.path.join("ses-*","*","*")) -> Dict[str,str]:
+	"""
 	Anonymizes all dicom images located at the datapath in the structure specified by subject_dicom_path parameter.
 	Args : 
 		output_folder (string) : path where anonymized images will be located.
@@ -62,12 +70,11 @@ def anonymize_all(output_folder = ".",datapath = os.path.join("..","data"), subj
 	Returns : 
 		dict : Dictionary keeping track of the new patientIDs and old patientIDs mappings.
 	"""
-
 	#Listing patient files.
 	patients_folders = next(os.walk(datapath))[1]
-	
+
 	old2new_idx = {patients_folders[i] : str(i).zfill(6) for i in range(len(patients_folders))}
-	
+
 	for patient in tqdm(patients_folders) : 
 		current_path = os.path.join(datapath, patient, subject_dicom_path)
 		files = glob(current_path)
@@ -75,9 +82,9 @@ def anonymize_all(output_folder = ".",datapath = os.path.join("..","data"), subj
 		for file in files:
 			
 			anonymize(file,os.path.join(output_folder,file), PatientID = old2new_idx[patient], PatientName = "FIFISIDKOM")
-		os.rename(os.path.join(datapath , patient), os.path.join(datapath,"sub-"+old2new_idx[patient]))
+		os.rename(os.path.join(datapath , patient), os.path.join(datapath,"sub-anony-"+old2new_idx[patient]))
 	new2old_idx = {new : old.replace("sub-","") for old, new in old2new_idx.items()}
-	
+
 	return new2old_idx
 
 def main(argv): 
