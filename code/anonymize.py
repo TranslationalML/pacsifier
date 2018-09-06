a9c4b0702f78de166d02c23cee6df1b7cef0d21d
firas11 <firas.benothman@epfl.ch>
2018-09-06 12:03:10 +0200

    fix potential bug

--- code/anonymize.py
+++ code/anonymize.py
@@ -8,16 +8,18 @@ import random
 import json
 
 def fuzz_date(date, fuzz_parameter = 60): 
-    
-    year = int(date[:4])
-    month = int(date[4:6])
-    day = int(date[6:8])
+	"""
+	
+	"""
+	year = int(date[:4])
+	month = int(date[4:6])
+	day = int(date[6:8])
 
-    fuzz = random.randint(-fuzz_parameter, fuzz_parameter)
+	fuzz = random.randint(-fuzz_parameter, fuzz_parameter)
 
-    date = datetime(day = day, month = month, year = year)
-    date = (date + timedelta(days = fuzz)).strftime("%Y%m%d")
-    return date
+	date = datetime(day = day, month = month, year = year)
+	date = (date + timedelta(days = fuzz)).strftime("%Y%m%d")
+	return date
 
 def anonymize(filename, output_filename, PatientID = "test1", PatientName = "test1"):
 	# Load the current dicom file to 'anonymize'
@@ -35,8 +37,10 @@ def anonymize(filename, output_filename, PatientID = "test1", PatientName = "tes
 	dataset.save_as(output_filename)
 
 def anonymize_all(output_folder = ".",datapath = os.path.join("..","data"), subject_dicom_path = os.path.join("ses-*","*","*")):
+	
+	#Listing patient files.
 	patients_folders = next(os.walk(datapath))[1]
-	#patient_ids = [patient_folder.split("-")[1].replace(" ","") for patient_folder in patients_folders]
+	
 	old2new_idx = {patients_folders[i] : str(i).zfill(6) for i in range(len(patients_folders))}
 	
 	for patient in tqdm(patients_folders) : 
@@ -47,15 +51,20 @@ def anonymize_all(output_folder = ".",datapath = os.path.join("..","data"), subj
 			
 			anonymize(file,os.path.join(output_folder,file), PatientID = old2new_idx[patient], PatientName = "FIFISIDKOM")
 		os.rename(os.path.join(datapath , patient), os.path.join(datapath,"sub-"+old2new_idx[patient]))
-	new2old_idx = {new : old.split("-")[1].replace(" ","") for old, new in old2new_idx.items()}
+	new2old_idx = {new : old.replace("sub-","") for old, new in old2new_idx.items()}
 	
 	return new2old_idx
 
 def main(argv): 
 	json_path = argv[0]
 	print("Anonymizing ...")
+
+	#Anonymizing all files.
 	mapper = anonymize_all()
+
+	#dumping new ids to a json file.
 	with open(os.path.join(json_path,'mapper.json'), 'w') as fp:
 		json.dump(mapper, fp)
+
 if __name__ == "__main__" : 
 	main(sys.argv[1:])
