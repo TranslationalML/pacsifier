929dbda45a1ef798c6e22494f4f9067eca95cc55
Tommaso Di Noto <tommydino@hotmail.it>
2020-06-12 08:55:47 +0000

    Update pacsman.py

--- pacsman.py
+++ pacsman.py
@@ -317,8 +317,8 @@ def retrieve_dicoms_using_table(table : DataFrame, parameters : Dict[str,str], o
 		for serie in tqdm(series) :
 		    
 		    if not serie["PatientID"].isalnum():  # if patient ID is not alphanumeric
-                    # replace illegal chars with underscores
-                    serie["PatientID"] = my_re_clean.sub('_', unicodedata.normalize('NFD', serie["PatientID"]).encode('ascii','ignore').decode('ascii'))
+                        # replace illegal chars with underscores
+                        serie["PatientID"] = my_re_clean.sub('_', unicodedata.normalize('NFD', serie["PatientID"]).encode('ascii','ignore').decode('ascii'))
 			
 			counter+=1
 			if counter % batch_size == 0: 
