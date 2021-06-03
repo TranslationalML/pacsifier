7f1c1ba888dd4294066ba84f869ef02428465b31
jonasrichiardi <jonas.richiardi@chuv.ch>
2021-06-03 11:26:02 +0200

    change album_name to project_name in get_pseudonyms

--- get_pseudonyms.py
+++ get_pseudonyms.py
@@ -16,17 +16,16 @@ def check_config_file_deid(config_file: Dict[str, str]) -> None:
         raise ValueError("Invalid config file! Must contain these and only these keys: " + ' '.join(valid_keys))
 
 
-def convert_csv_to_json(queryfile: str, album_name: str) -> Any:
+def convert_csv_to_json(queryfile: str, project_name: str) -> Any:
     """
     Convert PACSMAN query to json format the de-ID API can understand
     Args:
         queryfile: the filename of the PACSMAN query file
-        album_name: the name of the Kheops album the study will be sent to, equal to the name of the project in the
-            de-ID API
+        project_name: the name of the project in GPCR (may or may not correspond to Kheops album)
     Returns:
         JSON object suitable for the API
     """
-    json_new_str = '{"project": "' + album_name + '", "PatientIDList": ['
+    json_new_str = '{"project": "' + project_name + '", "PatientIDList": ['
     patient_list_str = ""
 
     with open(queryfile, newline='') as csvfile:
@@ -51,7 +50,7 @@ def main(argv):
     parser.add_argument('--config', "-c", help='Deidentification configuration file path',
                         default=os.path.join(".", "files", "config_deid.json"))
     parser.add_argument("--queryfile", "-q", help='Path to PACSMAN query file (used to find PatientIDs)')
-    parser.add_argument("--album_name", "-a", help='Name of the destination Kheops album (corresponds to project in de-ID API)')
+    parser.add_argument("--project_name", "-a", help='Name of the project in GPCR (may or may not correspond to Kheops album)')
     parser.add_argument("--out_directory", "-d", help='Output directory where the pseudonyms will be saved as a json',
                         default=os.path.join(".", "data"))
 
@@ -67,9 +66,9 @@ def main(argv):
 
     #TODO should enable reading IDs from json file too, or scan from disk.
 
-    # Check the case where the queryfile, config, or albumn_name is missing. If it is the case print help.
-    if args.queryfile is None or args.config is None or args.album_name is None:
-        print("Missing mandatory parameter --queryfile or --config or --album_name!")
+    # Check the case where the queryfile, config, or project_name is missing. If it is the case print help.
+    if args.queryfile is None or args.config is None or args.project_name is None:
+        print("Missing mandatory parameter --queryfile or --config or --project_name!")
         parser.print_help()
         sys.exit()
 
@@ -80,11 +79,11 @@ def main(argv):
     # build POST request
     head = {'Authorization': 'token {}'.format(parameters['deid_token'])}
 
-    json_data = convert_csv_to_json(args.queryfile, args.album_name)
+    json_data = convert_csv_to_json(args.queryfile, args.project_name)
     response = requests.post(parameters['deid_URL'], headers=head, json=json_data, verify=False)
 
     print(response.text)
-    with open(os.path.normpath(os.path.join(args.out_directory, "new_ids_{}.json".format(args.album_name))), "w") as f:
+    with open(os.path.normpath(os.path.join(args.out_directory, "new_ids_{}.json".format(args.project_name))), "w") as f:
         f.write(response.text)
 
 
