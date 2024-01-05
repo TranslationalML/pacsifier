c2a1be5f473e7a68803785eb72f10ceb25345b1d
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2024-01-05 18:35:32 +0100

    test: add / update test_anonymize and test_anonymize_all_dicoms_within_folder

--- tests/cli/test_anonymize_dicoms.py
+++ tests/cli/test_anonymize_dicoms.py
@@ -1,9 +1,18 @@
 """Tests for the functions of the anonymize_dicoms script.""" ""
 
+import os
 import random
+import shutil
+import sys
 import pytest
 
-from pacsman.cli.anonymize_dicoms import fuzz_date
+import pydicom
+
+from pacsman.cli.anonymize_dicoms import (
+    fuzz_date,
+    anonymize_dicom_file,
+    anonymize_all_dicoms_within_root_folder,
+)
 
 
 def test_fuzz_date():
@@ -14,139 +23,175 @@ def test_fuzz_date():
         fuzz_date("20180911", fuzz_parameter=random.randint(-10000, 0))
 
 
-# def test_anonymize():
-#     in_file = "/tests/test_data/rubo_sample_file_0003.DCM"
-#     out_file = "/tests/test_data/rubo_sample_file_0003_anon.DCM"
-#     anonymize_dicom_file(
-#         in_file,
-#         out_file,
-#         PatientID="00042",
-#         new_StudyInstanceUID="0000001.01",
-#         new_SeriesInstanceUID="000012.01",
-#         new_SOPInstanceUID="000000010.101",
-#         fuzz_birthdate=True,
-#         fuzz_acqdates=True,
-#         fuzz_days_shift=10,
-#         delete_identifiable_files=True,
-#         remove_private_tags=False,
-#     )
-#     dataset = pydicom.read_file(out_file)
-#     attributes = dataset.dir("")
+def test_anonymize(test_dir):
+    in_file = os.path.join(test_dir, "test_data", "dicomseries", "slice0.dcm")
+    out_dir = os.path.join(test_dir, "tmp", "test_data", "dicomseries_anon")
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir, exist_ok=True)
+    out_file = os.path.join(out_dir, "slice0.dcm")
+    anonymize_dicom_file(
+        in_file,
+        out_file,
+        PatientID="PACSMAN2",
+        new_StudyInstanceUID="0000001.01",
+        new_SeriesInstanceUID="000012.01",
+        new_SOPInstanceUID="000000010.101",
+        fuzz_birthdate=True,
+        fuzz_acqdates=True,
+        fuzz_days_shift=10,
+        delete_identifiable_files=True,
+        remove_private_tags=False,
+    )
+    dataset = pydicom.read_file(out_file)
+    attributes = dataset.dir("")
 
-#     assert dataset.PatientID == "00042"
-#     assert dataset.PatientName == "00042^sub"
-#     assert dataset.InstitutionAddress == "Address"
-#     assert (
-#         dataset.PatientBirthDate == "19580729"
-#     )  # original date PatientBirthDate 19580719 -> check + 10 days shift
-#     assert dataset.StudyDate == "19930404"  # original StudyDate 19930325
-#     if "ReferringPhysicianTelephoneNumbers" in attributes:
-#         assert dataset.ReferringPhysicianTelephoneNumbers == ""
-#     if "PatientTelephoneNumbers" in attributes:
-#         assert dataset.PatientTelephoneNumbers == ""
-#     if "PersonTelephoneNumbers" in attributes:
-#         assert dataset.PersonTelephoneNumbers == ""
-#     if "orderCallbackPhoneNumber" in attributes:
-#         assert dataset.OrderCallbackPhoneNumber == ""
-#     if "InstitutionName" in attributes:
-#         assert dataset.InstitutionName == ""
+    assert dataset.PatientID == "PACSMAN2"
+    assert dataset.PatientName == "PACSMAN2^sub"
+    if "InstitutionAddress" in attributes:
+        assert dataset.InstitutionAddress == "Address"
+    if "PatientBirthDate" in attributes:
+        assert (
+            dataset.PatientBirthDate == "20231026"
+        )  # original date PatientBirthDate 20231016 -> check + 10 days shift
+    assert (
+        dataset.StudyDate == "20231026"
+    )  # original StudyDate 20231016 -> check + 10 days shift
+    if "ReferringPhysicianTelephoneNumbers" in attributes:
+        assert dataset.ReferringPhysicianTelephoneNumbers == ""
+    if "PatientTelephoneNumbers" in attributes:
+        assert dataset.PatientTelephoneNumbers == ""
+    if "PersonTelephoneNumbers" in attributes:
+        assert dataset.PersonTelephoneNumbers == ""
+    if "orderCallbackPhoneNumber" in attributes:
+        assert dataset.OrderCallbackPhoneNumber == ""
+    if "InstitutionName" in attributes:
+        assert dataset.InstitutionName == ""
 
-#     if "ReferringPhysiciansAddress" in attributes:
-#         assert dataset.ReferringPhysiciansAddress == ""
+    if "ReferringPhysiciansAddress" in attributes:
+        assert dataset.ReferringPhysiciansAddress == ""
 
-#     if "ReferringPhysicianIDSequence" in attributes:
-#         assert dataset.ReferringPhysicianIDSequence == ""
+    if "ReferringPhysicianIDSequence" in attributes:
+        assert dataset.ReferringPhysicianIDSequence == ""
 
-#     if "InstitutionalDepartmentName" in attributes:
-#         assert dataset.InstitutionalDepartmentName == ""
+    if "InstitutionalDepartmentName" in attributes:
+        assert dataset.InstitutionalDepartmentName == ""
 
-#     if "PhysicianOfRecord" in attributes:
-#         assert dataset.PhysicianOfRecord == ""
+    if "PhysicianOfRecord" in attributes:
+        assert dataset.PhysicianOfRecord == ""
 
-#     if "PerformingPhysicianName" in attributes:
-#         assert dataset.PerformingPhysicianName == ""
+    if "PerformingPhysicianName" in attributes:
+        assert dataset.PerformingPhysicianName == ""
 
-#     if "ReferringPhysiciansAddress" in attributes:
-#         assert dataset.ReferringPhysiciansAddress == ""
+    if "ReferringPhysiciansAddress" in attributes:
+        assert dataset.ReferringPhysiciansAddress == ""
 
-#     if "ReferringPhysiciansTelephoneNumber" in attributes:
-#         assert dataset.ReferringPhysiciansTelephoneNumber == ""
+    if "ReferringPhysiciansTelephoneNumber" in attributes:
+        assert dataset.ReferringPhysiciansTelephoneNumber == ""
 
-#     if "PerformingPhysicianIDSequence" in attributes:
-#         assert dataset.PerformingPhysicianIDSequence == ""
+    if "PerformingPhysicianIDSequence" in attributes:
+        assert dataset.PerformingPhysicianIDSequence == ""
 
-#     if "NameOfPhysicianReadingStudy" in attributes:
-#         assert dataset.NameOfPhysicianReadingStudy == ""
+    if "NameOfPhysicianReadingStudy" in attributes:
+        assert dataset.NameOfPhysicianReadingStudy == ""
 
-#     if "PhysicianReadingStudyIDSequence" in attributes:
-#         assert dataset.PhysicianReadingStudyIDSequence == ""
+    if "PhysicianReadingStudyIDSequence" in attributes:
+        assert dataset.PhysicianReadingStudyIDSequence == ""
 
-#     if "OperatorsName" in attributes:
-#         assert dataset.OperatorsName == ""
+    if "OperatorsName" in attributes:
+        assert dataset.OperatorsName == ""
 
-#     if "IssuerOfPatientID" in attributes:
-#         assert dataset.IssuerOfPatientID == ""
+    if "IssuerOfPatientID" in attributes:
+        assert dataset.IssuerOfPatientID == ""
 
-#     if "PatientsBirthTime" in attributes:
-#         assert dataset.PatientsBirthTime == ""
+    if "PatientsBirthTime" in attributes:
+        assert dataset.PatientsBirthTime == ""
 
-#     if "OtherPatientIDs" in attributes:
-#         assert dataset.OtherPatientIDs == ""
+    if "OtherPatientIDs" in attributes:
+        assert dataset.OtherPatientIDs == ""
 
-#     if "OtherPatientNames" in attributes:
-#         assert dataset.OtherPatientNames == ""
+    if "OtherPatientNames" in attributes:
+        assert dataset.OtherPatientNames == ""
 
-#     if "PatientBirthName" in attributes:
-#         assert dataset.PatientBirthName == ""
+    if "PatientBirthName" in attributes:
+        assert dataset.PatientBirthName == ""
 
-#     if "PersonAddress" in attributes:
-#         assert dataset.PersonAddress == ""
+    if "PersonAddress" in attributes:
+        assert dataset.PersonAddress == ""
 
-#     if "PatientMotherBirthName" in attributes:
-#         assert dataset.PatientMotherBirthName == ""
+    if "PatientMotherBirthName" in attributes:
+        assert dataset.PatientMotherBirthName == ""
 
-#     if "CountryOfResidence" in attributes:
-#         assert dataset.CountryOfResidence == ""
+    if "CountryOfResidence" in attributes:
+        assert dataset.CountryOfResidence == ""
 
-#     if "RegionOfResidence" in attributes:
-#         assert dataset.RegionOfResidence == ""
+    if "RegionOfResidence" in attributes:
+        assert dataset.RegionOfResidence == ""
 
-#     if "CurrentPatientLocation" in attributes:
-#         assert dataset.CurrentPatientLocation == ""
+    if "CurrentPatientLocation" in attributes:
+        assert dataset.CurrentPatientLocation == ""
 
-#     if "PatientInstitutionResidence" in attributes:
-#         assert dataset.PatientsInstitutionResidence == ""
+    if "PatientInstitutionResidence" in attributes:
+        assert dataset.PatientsInstitutionResidence == ""
 
-#     if "PersonName" in attributes:
-#         assert dataset.PersonName == ""
+    if "PersonName" in attributes:
+        assert dataset.PersonName == ""
 
-#     dataset.PatientAge = "091"
-#     dataset.save_as("/tests/test_data/temp_output_sample_image.DCM")
+    # Test if age is properly handled
+    in_file_new_age = os.path.join(
+        test_dir, "tmp", "test_data", "dicomseries_anon", "slice0_newage.dcm"
+    )
+    out_file_new_age = os.path.join(
+        test_dir, "tmp", "test_data", "dicomseries_anon", "slice0_newage_anon.dcm"
+    )
 
-#     anonymize_dicom_file(
-#         "/tests/test_data/temp_output_sample_image.DCM",
-#         "/tests/test_data/temp_output_sample_image.DCM",
-#         PatientID="00042",
-#         new_StudyInstanceUID="0000001.01",
-#         new_SeriesInstanceUID="000012.01",
-#         new_SOPInstanceUID="000000010.101",
-#         fuzz_birthdate=True,
-#         fuzz_acqdates=False,
-#         fuzz_days_shift=5,
-#         delete_identifiable_files=True,
-#         remove_private_tags=False,
-#     )
+    if os.path.exists(in_file_new_age):
+        os.remove(in_file_new_age)
+    if os.path.exists(out_file_new_age):
+        os.remove(out_file_new_age)
 
-#     dataset = pydicom.read_file("/tests/test_data/temp_output_sample_image.DCM")
-#     assert dataset.PatientAge == "90+Y"
+    dataset.PatientAge = "091"
+    dataset.save_as(in_file_new_age)
+
+    anonymize_dicom_file(
+        in_file_new_age,
+        out_file_new_age,
+        PatientID="PACSMAN2",
+        new_StudyInstanceUID="0000001.01",
+        new_SeriesInstanceUID="000012.01",
+        new_SOPInstanceUID="000000010.101",
+        fuzz_birthdate=True,
+        fuzz_acqdates=False,
+        fuzz_days_shift=5,
+        delete_identifiable_files=True,
+        remove_private_tags=False,
+    )
+
+    dataset = pydicom.read_file(out_file_new_age)
+    assert dataset.PatientAge == "90+Y"
 
 
-# def test_anonymize_all_dicoms_within_folder():
-#     dict_ = anonymize_all_dicoms_within_root_folder(
-#         output_folder=".",
-#         datapath=".",
-#         pattern_dicom_files="output_sample_image",
-#         rename_patient_directories=False,
-#     )
-#     # print(dict_)
-#     # assert dict_ == {'000003': '__pycache__', '000002': 'test_set', '000000': '.pytest_cache', '000001': '.hypothesis'}
+def test_anonymize_all_dicoms_within_folder(test_dir):
+    # Create a folder with a few files following the expected structure
+    dicom_dir = os.path.join(test_dir, "test_data", "dicomseries")
+    pacsman_dir = os.path.join(test_dir, "tmp", "test_data", "dicomseries_structured")
+    structured_series_dir = os.path.join(pacsman_dir, "sub-PACSMAN1", "ses-20232016", "00000-No_series_description")
+    anonymization_dir = os.path.join(test_dir, "tmp", "test_data", "dicomseries_structured_anon")
+
+    for dir in [structured_series_dir, anonymization_dir]:
+        if os.path.exists(dir):
+            shutil.rmtree(dir, ignore_errors=True)
+        os.makedirs(dir, exist_ok=True)    
+
+    for dir, _, files in os.walk(dicom_dir):
+        for f in files:
+            print(f"dir = {dir}, f = {f}", file=sys.stderr)
+            if f.endswith(".dcm"):
+                shutil.copyfile(os.path.join(dir, f), os.path.join(structured_series_dir, f))
+
+    dict_ = anonymize_all_dicoms_within_root_folder(
+        output_folder=anonymization_dir,
+        datapath=pacsman_dir,
+        pattern_dicom_files=os.path.join("ses-*", "*", "*.dcm"),
+        rename_patient_directories=False,
+    )
+    assert dict_ == {'000001': 'PACSMAN1'}
