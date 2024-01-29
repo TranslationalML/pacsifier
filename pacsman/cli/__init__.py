57b4e242a5f36ed044acff8f09245a91cbcf9c58
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2024-01-29 14:53:11 +0100

    chore: remove import from deleted convert cli script in cli/__init__.py

--- pacsman/cli/__init__.py
+++ pacsman/cli/__init__.py
@@ -1,6 +1,5 @@
 from .add_karnak_tags import *
 from .anonymize_dicoms import *
-from .convert import *
 from .create_dicomdir import *
 from .extract_carestream_report import *
 from .get_pseudonyms import *
