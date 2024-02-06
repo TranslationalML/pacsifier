7bdc07717da6b36d942b337b6a8f85d13f6a6dca
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2024-02-06 10:05:15 +0100

    feat(get_pseudonyms.py): implement generation of new_ids and day_shift json file from custom mapping file

--- pacsman/cli/get_pseudonyms.py
+++ pacsman/cli/get_pseudonyms.py
@@ -19,6 +19,7 @@ import ast
 import sys, os, argparse
 import json, csv, requests
 from typing import Dict, Any
+import numpy as np
 from pandas import read_csv
 from pacsman.cli.pacsman import check_query_table_allowed_filters
 
@@ -76,12 +77,12 @@ def get_deid_pseudonyms(deid_parameters: dict, query_json: dict) -> None:
 
     Args:
         deid_parameters: dictionary containing the de-ID URL and token in the following format::
-            
+
             {
                 "deid_URL": "https://deid.gpcr-project.org",
                 "deid_token": "1234567890"
             }
-        
+
         query_json: the PACSMAN query formatted as a dictionary
 
     Returns:
@@ -95,7 +96,7 @@ def get_deid_pseudonyms(deid_parameters: dict, query_json: dict) -> None:
     )
 
     resp = ast.literal_eval(response.text)
-    json_resp = json.dumps(resp)
+    json_resp = json.dumps(resp, indent=4)
 
     return json_resp
 
@@ -105,12 +106,12 @@ def get_deid_day_shifts(deid_parameters: dict, query_json: dict) -> None:
 
     Args:
         deid_parameters: dictionary containing the de-ID URL and token in the following format::
-            
+
             {
                 "deid_URL": "https://deid.gpcr-project.org",
                 "deid_token": "1234567890"
             }
-        
+
         query_json: the PACSMAN query formatted as a dictionary
 
     Returns:
@@ -120,13 +121,32 @@ def get_deid_day_shifts(deid_parameters: dict, query_json: dict) -> None:
     head = {"Authorization": f'token {deid_parameters["deid_token"]}'}
 
     response = requests.post(
-        deid_parameters["deid_URL"] + "_shift", headers=head, json=query_json, verify=False
+        deid_parameters["deid_URL"] + "_shift",
+        headers=head,
+        json=query_json,
+        verify=False,
     )
 
     resp = ast.literal_eval(response.text)
-    json_resp = json.dumps(resp)
-    
+    json_resp = json.dumps(resp, indent=4)
+
     return json_resp
+
+
+def check_queryfile_content(queryfile: str) -> None:
+    """Check that the PACSMAN query file is valid.
+
+    Args:
+        queryfile: the path of the PACSMAN query file
+
+    """
+    try:
+        query_table = read_csv(queryfile, dtype=str).fillna("")
+    except Exception as e:
+        print(f"Error reading PACSMAN query file: {e}")
+        sys.exit(1)
+
+    check_query_table_allowed_filters(query_table)
 
 
 def get_parser() -> argparse.ArgumentParser:
@@ -197,68 +217,90 @@ def main():
     parser = get_parser()
     args = parser.parse_args()
 
-    # Check the case where the queryfile, config, or project_name is missing.
-    # If it is the case print help.
-    # TODO: Can be directly done with argparse with the required=True option.
-    if args.queryfile is None or args.config is None or args.project_name is None:
-        print("Missing mandatory parameter --queryfile or --config or --project_name!")
-        parser.print_help()
-        sys.exit()
-
-    # Check if the config file exists.
-    config_path = os.path.abspath(args.config)
-    if not os.path.isfile(config_path):
-        print(f"Config file {config_path} does not exist!")
-        sys.exit()
-    
     # Check if the output directory exists. If not, create it.
     output_dir = os.path.abspath(args.out_directory)
     if not os.path.isdir(output_dir):
-        print(f"Output directory {output_dir} does not exist! Creating...")
+        print(f"Output directory does not exist! Creating {output_dir}...")
         os.makedirs(output_dir, exist_ok=True)
 
-    # Check if the queryfile exists.
-    query_file = os.path.abspath(args.queryfile)
-    if not os.path.isfile(query_file):
-        print(f"Query file {query_file} does not exist!")
-        sys.exit()
+    if args.mode == "de-id":
+        # Check if the config file exists.
+        config_path = os.path.abspath(args.config)
+        if not os.path.isfile(config_path):
+            print(f"Config file {config_path} does not exist!")
+            sys.exit(1)
 
-    # Read and validate the queryfile.
-    table = read_csv(args.queryfile, dtype=str).fillna("")
-    check_query_table_allowed_filters(table)
+        # Read and validate config file for deid parameters.
+        with open(config_path) as f:
+            try:
+                deid_parameters = json.load(f)
+            except json.JSONDecodeError:
+                print("The config file does not contain valid json.")
+                sys.exit(1)
 
-    # Reading config file for deid parameters.
-    with open(config_path) as f:
-        deid_parameters = json.load(f)
-        check_config_file_deid(deid_parameters)
-    
-    # TODO should enable reading IDs from json file too, or scan from disk.
+            check_config_file_deid(deid_parameters)
 
-    # Convert the queryfile to json format.
-    query_json = convert_csv_to_deid_json(query_file, args.project_name)
+        # Check if the queryfile exists.
+        query_file = os.path.abspath(args.queryfile)
+        if not os.path.isfile(query_file):
+            print(f"Query file {query_file} does not exist!")
+            sys.exit(1)
 
-    # Get the new pseudonyms
-    json_response = get_deid_pseudonyms(deid_parameters, query_json)
-    print(json_response)
+        # Validate the queryfile content.
+        check_queryfile_content(query_file)
 
-    with open(
-        os.path.normpath(
-            os.path.join(output_dir, f"new_ids_{args.project_name}.json")
-        ),
-        "w",
-    ) as f:
-        f.write(json_response)
+        # Convert the queryfile to json format.
+        deid_query_json = convert_csv_to_deid_json(query_file, args.project_name)
 
-    # Get day shift
-    json_response = get_deid_day_shifts(deid_parameters, query_json)
+        # Get the new pseudonyms
+        json_pseudo_response = get_deid_pseudonyms(deid_parameters, deid_query_json)
 
-    with open(
-        os.path.normpath(
-            os.path.join(output_dir, f"day_shift_{args.project_name}.json")
-        ),
-        "w",
-    ) as f:
-        f.write(json_response)
+        # Get day shift
+        json_day_shift_response = get_deid_day_shifts(deid_parameters, deid_query_json)
+
+    else:
+        # Read the custom mapping file.
+        mappingfile_path = os.path.abspath(args.mappingfile)
+        if not os.path.isfile(mappingfile_path):
+            print(f"Custom mapping file {mappingfile_path} does not exist!")
+            sys.exit(1)
+
+        mapping_table = read_csv(mappingfile_path, dtype=str, header=None)
+        if mapping_table.isnull().values.any():
+            print("The custom mapping file contains empty cells!")
+            sys.exit(1)
+
+        pseudo_mapping = {row[0]: row[1] for row in mapping_table.to_numpy(dtype=str)}
+
+        # Convert the custom pseudo mapping to json format.
+        json_pseudo_response = json.dumps(pseudo_mapping, indent=4)
+
+        # Generate random day shifts between -30 and 30 days for all pseudonyms
+        # if specified. Otherwise, set day shifts to 0.
+        day_shifts = {
+            p: np.random.randint(-30, 31) if args.shift_days else 0
+            for p in pseudo_mapping.values()
+        }
+        json_day_shift_response = json.dumps(day_shifts, indent=4)
+
+    # Print the old / new pseudonyms and day shifts if verbose mode is on.
+    if args.verbose:
+        print(f"Old / new pseudonyms: {json_pseudo_response} \n")
+        print(f"Day shifts: {json_day_shift_response}")
+
+    # Save the old /new pseudonyms mapping to a json file.
+    json_pseudo_response_path = os.path.normpath(
+        os.path.join(output_dir, f"new_ids_{args.project_name}.json")
+    )
+    with open(json_pseudo_response_path, "w") as f:
+        f.write(json_pseudo_response)
+
+    # Save the day shifts to a json file.
+    json_day_shift_response_path = os.path.normpath(
+        os.path.join(output_dir, f"day_shift_{args.project_name}.json")
+    )
+    with open(json_day_shift_response_path, "w") as f:
+        f.write(json_day_shift_response)
 
     print("Done!")
 
